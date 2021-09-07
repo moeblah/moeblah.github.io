585dfb9b3a693e72eb97d3a2074b417da46b72f5
MoGi <mogiblahblah@gmail.com>
2021-09-07 13:01:55 +0900

    .

--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,2 +0,0 @@
-<template><h1 id="hello-moe-documentation-page" tabindex="-1"><a class="header-anchor" href="#hello-moe-documentation-page" aria-hidden="true">#</a> Hello Moe Documentation page</h1>
-</template>
